958802780d52c6c35fe735912ee631531544ff81
LuisEJaar <luisjaarcoding@gmail.com>
2022-05-14 20:49:01 -0500

    Refactored Equal Sides of an Array, happy with new code

--- ongoingCodewars/6kyu/Equal Sides of An Array.js
+++ ongoingCodewars/6kyu/Equal Sides of An Array.js
@@ -1,22 +1,16 @@
 function findEvenIndex(arr) {
-    if ((arr.length === 2 && arr[1] === 0) || arr.length === 1) {
-        return 0
+  for(i = 0; i < arr.length; i++) {
+    //test for edge cases with one value or the pivot is at the end
+    if (arr.length === 1 || 0 === arr.slice(0,arr.length-1).reduce((a,b) => a + b)) {
+      return arr.length - 1
     }
-    if (arr.length === 2 && arr[0] === 0) {
-        return 1
+    
+    //compare lh to rh of pivot
+    let tempL = (i == 0) ? 0 : arr.slice(0,i).reduce((a,b) => a + b)
+    let tempR = (i == arr.length - 1) ? 1: arr.slice(i+1,arr.length).reduce((a,b) => a + b)
+    if(tempL == tempR) {
+      return i
     }
-    for(i = 1; i < arr.length-1; i++) {
-      if (0 == arr.slice(0,arr.length-1).reduce((a,b) => a + b)) {
-        return arr.length-1
-      }
-      if (arr[0] === arr.slice(0).reduce((a,b) => a + b)) {
-        return 0
-      } 
-      let tempL = arr.slice(0,i).reduce((a,b) => a + b)
-      let tempR = arr.slice(i+1,arr.length).reduce((a,b) => a + b)
-      if(tempL == tempR) {
-        let answer = i
-        return answer
-      }
-    }
-    return -1
+  }
+  return -1
+}
