5d7947c76f586e94745aa3a7e2096e411b782606
LuisEJaar <luisjaarcoding@gmail.com>
2022-05-26 12:03:32 -0500

    Refactor of count ip addresses

--- ongoingCodewars/5kyu/count ip addresses.js
+++ ongoingCodewars/5kyu/count ip addresses.js
@@ -2,16 +2,10 @@ function ipsBetween(start, end){
     let startArr = start.split(".")
     let endArr = end.split(".")
     let diffArr = []
-    let translation = {
-      0: 16777216, 
-      1: 65536,
-      2: 256,
-      3: 1
-    }
     
     for(i = 0; i < startArr.length; i++) {
       diffArr.push(endArr[i] - startArr[i])  
     }
     
-   return diffArr.map((entry, index) => entry * translation[index]).reduce((a,b)=> a+b)
+   return diffArr.reverse().map((entry, index) => entry * 256 ** index).reduce((a,b)=> a+b)
   }
