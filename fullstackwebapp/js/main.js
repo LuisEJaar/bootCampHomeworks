d03149168300e602c6131bc2b361284adfa76e82
LuisEJaar <luisjaarcoding@gmail.com>
2022-05-31 10:09:52 -0500

    Updated CSS for webapp

--- fullstackwebapp/js/main.js
+++ fullstackwebapp/js/main.js
@@ -5,6 +5,14 @@ async function makeReq(){
   const res = await fetch(`/api?`)
   const data = await res.json()
 
-  console.log(data);
-  document.querySelector("#catFact").textContent = data
+  //Out with the old
+  const cardContainer = document.querySelector('.card')
+  const oldCard = document.querySelector('#catFact')
+  cardContainer.removeChild(oldCard)
+
+  //In with the new
+  const newFact = document.createElement('h2')
+  newFact.setAttribute('id','catFact')
+  newFact.textContent = data
+  document.querySelector('.card').appendChild(newFact)
 }
