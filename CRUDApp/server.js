c9dc77ff1d804ebc0e6c41cbf50f0c096c3c4a99
LuisEJaar <luisjaarcoding@gmail.com>
2022-06-02 09:18:05 -0500

    Pushing working copy

--- CRUDApp/server.js
+++ CRUDApp/server.js
@@ -1,3 +1,4 @@
+
 console.log("May the Node be with you")
 
 const express = require('express')
@@ -8,52 +9,31 @@ app.listen(3000, function() {
     console.log('listening on 3000')
 })
 
-// Make sure you place body-parser before your CRUD handlers!
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// The endpoint is whatever is after the domain
-// the callback is what happens when we receive a given
-// endpoint. 
-// app.get(endpoint, callback)
-
-// app.get('/', function(req, res) {
-//     res.send('Hello World')
-// })
-
-//Same as above, but more likely to see
-
-app.get('/', (req, res) => {
-    // res.send('Hello World')
-    res.sendFile(__dirname + '/index.html')
-})
-
-// the res.sendFile lets you return a file
-
-//The /quotes value in the form action attribute will trigger
-//this post. 
-
-//req body lets us see what was entered "request . body"
-app.post('/quotes', (req, res) => {
-    console.log('Hellooooooooooooooooo!')
-    console.log(req.body)
-  })
-
-//mongo time!
 const MongoClient = require('mongodb').MongoClient
 
 let connectionString = 'mongodb+srv://LuisJaar:3fnDL9CTjnsrMFn@cluster0.jjrkk.mongodb.net/?retryWrites=true&w=majority'
-//showing as a callback
-// MongoClient.connect(connectionString, {
-//     useUnifiedTopology: true
-//   }, (err, client) => {
-//     if (err) return console.error(err)
-//     console.log('Connected to Database')
-//   })
 
-
-//showing as a promise
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
+MongoClient.connect(connectionString, { useUnifiedTopology: true }) 
 .then(client => {
-console.log('Connected to Database')
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+    app.set('view engine', 'ejs')
+    app.get('/', (req, res) => {
+        db.collection('quotes').find().toArray()
+          .then(results => {
+            res.render('index.ejs', { quotes: results })
+          })
+          .catch(error => console.error(error))
+      })
+    app.post('/quotes', (req, res) => {
+        quotesCollection.insertOne(req.body)
+          .then(result => {
+            res.redirect('/')
+          })
+          .catch(error => console.error(error))
+      })
 })
 .catch(error => console.error(error))
